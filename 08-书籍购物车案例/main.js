3874f90d2e38c59e49a832a4833c762659044f57
Neio <1126819198@qq.com>
2019-10-14 09:29:27 +0800

    Vue2.5基础学习笔记: 1.ES6基础语法 2.插值操作与指令 3.动态绑定属性 4.计算属性 5.监听事件与参数修饰符 6.条件判断 7.循环与遍历 8.书籍购物车案例 9.表单常用指令v-model 10.Vue核心技术组件化

--- 08-书籍购物车案例/main.js
+++ 08-书籍购物车案例/main.js
@@ -32,11 +32,35 @@ const app = new Vue({
       }
       ]},
   methods:{
+    // books对象的数量增加与减方法,需要索引传入来确定是book集合中的具体哪个对象
+    increment(index){
+      this.books[index].count++
+    },
+    decrement(index){
+      this.books[index].count--
+    },
+    removeHandle(index){
+      //vue的splice可以移除,可以替换;这里移除当前,传入当前索引,以及需要移除1位
+      this.books.splice(index,1)
+    },
     // 通过方法对数字进行过滤
     getFinalPrice(price){
       return "$"+price.toFixed(2)
     }
   },
+  //vue的访问属性
+  computed:{
+    //在这里写方法计算总价格
+    totalPrice(){
+      let totalPrice=0
+      //for循环遍历,books集合的全部对象,让其价格与数量属性相乘并相加
+      for (let i =0;i<this.books.length;i++){
+        totalPrice+=this.books[i].price*this.books[i].count
+      }
+      return totalPrice
+    }
+  },
+  //vue的过滤器
   filters:{
     showPrice(price){
       return "$"+price.toFixed(2)
